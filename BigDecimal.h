5aaf95d2c18c27978d71400c81cfe9090ad04b40
Kartoshkasmyasom <66018181+Kartoshkasmyasom@users.noreply.github.com>
2025-03-27 01:06:48 +0300

    Update BigDecimal.h

--- BigDecimal.h
+++ BigDecimal.h
@@ -7,19 +7,20 @@
 #include <cmath>
 #include <algorithm>
 #include <numeric>
+#include <sstream>
+#include <iomanip>
+
 
 class BigDecimal {
 private:
 	std::vector<long long> big_integer, big_fraction;
-	bool sign = 0;
+	bool sign = 0; // 1 - negative, 0 - positive
 	int base = 32, exp_starts, integer, fraction;
 public:
 
 	BigDecimal();
 
 	BigDecimal(std::string source, int given_integer = 8, int given_fraction = 8);
-	
-	BigDecimal(double source);
 	
 //	BigDecimal(long source);
 
@@ -34,11 +35,26 @@ public:
 	bool compare_absolute_values(const BigDecimal& other) const;
 
 	bool operator==(const BigDecimal& other) const;
+	bool operator!=(const BigDecimal& other) const;
+	bool operator<(const BigDecimal& other) const;
+	bool operator>(const BigDecimal& other) const;
+	bool operator<=(const BigDecimal& other) const;
+	bool operator>=(const BigDecimal& other) const;
+
+	const BigDecimal operator*(const BigDecimal& b) const;
+	const BigDecimal operator/(const BigDecimal& b) const;
+	void operator *=(const BigDecimal& b);
+	void operator /=(const BigDecimal& b);
+	
 
 	const BigDecimal operator+(const BigDecimal& b) const;
 	const BigDecimal operator-(const BigDecimal& b) const;
+	void operator+=(const BigDecimal& b);
+	void operator-=(const BigDecimal& b);
+
 
 	virtual ~BigDecimal();
 };
 
+BigDecimal operator""_longnum(long double number);
 #endif // BIG_DECIMAL_H
