c8b03b0f0a2acfa1497ebf418bff26586b2c56e4
Kartoshkasmyasom <66018181+Kartoshkasmyasom@users.noreply.github.com>
2025-03-27 22:53:43 +0300

    Update tests.cpp

--- Calculator/tests.cpp
+++ Calculator/tests.cpp
@@ -4,6 +4,7 @@
 // #include "Expression.cpp"
 // #include "Parser.h"
 // #include <memory>
+// #include <iostream>
 // using namespace std;
 //
 // bool test1() {
@@ -43,17 +44,46 @@
 //     return (expr1->apply(params) == 10)&& (expr2->apply(params) == 30) && (expr3->apply(params) == 0.5) && (expr4->apply(variables) == -15) && (expr->apply(variables1) == sin(-1));
 //
 // }
+// // runtime check
+// bool test3() {
+//     Parser<double> parser1("x * sin(x)"), parser2("x * sin(ln(cos(x)))");
+//     map<string, double> variables1 = {{"x", 1.5}};
+//     auto expr = parser1.parse();
+//     cout<<expr->apply(variables1)<<endl;
+//     return true;
+// }
 //
+// bool test_differ() {
+//     auto expr = make_shared<NumExpression<double>>(5.0);
+//     auto derivative = expr->differentiate("x");
 //
+//     return (derivative->get_value() == 0);
+// }
 //
-// // int main() {
-// //     if (test1()) {
-// //         std::cout << "OK\n";
-// //     }else
-// //         std::cout << "FAIL\n";
-// //     if (test2())
-// //         std::cout << "OK\n";
-// //     else
-// //         std::cout << "FAIL\n";
-// //     return 0;
-// // }
+// bool test_differ_var() {
+//     auto expr1 = make_shared<VarExpression<double>>("x");
+//     auto derivative1 = expr1->differentiate("x");
+//     auto derivative2 = expr1->differentiate("y");
+//     return (derivative1->get_value() == 1 && derivative2->get_value() == 0);
+// }
+//
+// int main() {
+//     if (test1()) {
+//         cout << "OK\n";
+//     }else
+//         cout << "FAIL\n";
+//     if (test2())
+//         cout << "OK\n";
+//     else
+//         cout << "FAIL\n";
+//     test3();
+//     if (test_differ())
+//         cout << "OK\n";
+//     else
+//         cout<<"FAIL\n";
+//     if (test_differ_var())
+//         cout << "OK\n";
+//     else
+//         cout<<"FAIL\n";
+//     return 0;
+// }
