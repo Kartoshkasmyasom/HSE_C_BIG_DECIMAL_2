7f85ada09cd03731c614452d2a9e53f48abb4124
Kartoshkasmyasom <66018181+Kartoshkasmyasom@users.noreply.github.com>
2025-03-27 22:53:20 +0300

    Update main.cpp

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
     if (argc < 3) {
         cerr << "Usage: " << argv[0] << "--command [expression] [var=value ...]" << endl;
         return 1;
@@ -12,13 +12,10 @@ int main(int argc, char* argv[]) {
 
     string command = argv[1];
     string input = argv[2];
+    //cout<<input<<endl;
 
     Parser<double> parser(input);
     if (command == "--eval") {
-        if (argc <= 2) {
-            cerr << "Usage: " << argv[0] << "--command [expression] [var=value ...]" << endl;
-            return 1;
-        }
         try {
             map<string, double> variables;
             for (int i = 3; i < argc; ++i) {
@@ -30,15 +27,42 @@ int main(int argc, char* argv[]) {
                     variables[var] = val;
                 }
             }
+            for (auto i : variables) {
+                cout<<i.first<<"="<<i.second<<endl;
+            }
             auto expr = parser.parse();
             cout << "Result: " << expr->apply(variables) << endl;
-            // auto derivative = expr->differentiate("x");
-            // cout << "Derivative with respect to x: " << derivative->evaluate(variables) << endl;
-        } catch (const exception& e) {
+        } catch (const exception &e) {
             cerr << "Error: " << e.what() << endl;
         }
-    }
+    } else if (command == "--diff") {
+        if (argc < 5 || string(argv[3]) != "--by") {
+            cerr << "Usage: " << argv[0] << " --diff [expression] --by [var] [var=value ...]" << endl;
+            return 1;
+        }
+        string var = argv[4];
+        try {
+            // map<string, double> variables;
+            // for (int i = 5; i < argc; ++i) {
+            //     string arg = argv[i];
+            //     int pos = arg.find('=');
+            //     if (pos != -1) {
+            //         string var_name = arg.substr(0, pos);
+            //         double val = stod(arg.substr(pos + 1));
+            //         variables[var_name] = val;
+            //     }
+            // }
 
+            auto expr = parser.parse();
+            auto derivative = expr->differentiate(var);
+            cout<<derivative->print()<<endl;
+        } catch (const exception &e) {
+            cerr << "Error: " << e.what() << endl;
+        }
+    } else {
+        cerr << "Unknown command: " << command << endl;
+        return 1;
+    }
 
 
     return 0;
